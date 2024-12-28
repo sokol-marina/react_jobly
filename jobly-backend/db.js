fe9b8e7d5b80d59744d46421bbf9458d4944a074
Marina Sukhova <sukhovaMarina12@gmail.com>
2024-12-27 20:23:23 -0800

    Deploy Jobly [backend|frontend]

--- jobly-backend/db.js
+++ jobly-backend/db.js
@@ -1,20 +1,24 @@
-"use strict";
+'use strict';
 /** Database setup for jobly. */
-const { Client } = require("pg");
-const { getDatabaseUri } = require("./config");
+const { Client } = require('pg');
+const { getDatabaseUri } = require('./config');
 
 let db;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   db = new Client({
     connectionString: getDatabaseUri(),
     ssl: {
-      rejectUnauthorized: false
-    }
+      rejectUnauthorized: false,
+    },
   });
 } else {
   db = new Client({
-    connectionString: getDatabaseUri()
+    connectionString: process.env.DATABASE_URL,
+    ssl:
+      process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false, // Disable SSL in development
   });
 }
 
